80e43c7444b06a3fcac82db7e071d77738629c3e
Manikprabhu1999 <mbjewargi@gmail.com>
2022-10-30 20:55:22 +0530

    Added:switch case

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,25 +3,32 @@ package com.bridgelabz;
 public class EmployeeWage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        //Constants
-		int empWage=0;
-		int empHrs;
+		
 		int EMP_RATE_PER_HOUR=20;
 		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
-		//Computation
-double empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == IS_FULL_TIME)
-	{
-	empHrs=16;
-	}
-	
-else
-    {
-    empHrs=8;
-    }
-empWage=empHrs*EMP_RATE_PER_HOUR;
-System.out.println("emp wage:"+empWage);
+		int IS_PART_TIME = 0;
+		int Full_day_hour =8;
+		int empWage =0;
+		int empHrs =0;
+		
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        
+        int i=(int)empCheck;
+        switch(i)
+        {
+        case 0:
+        	empHrs=8;
+        	break;
+        case 1:
+        	empHrs=16;
+        	break;
+       
+        default:
+        	empHrs=0;
+        	break;	
+        }
+        
+       empWage=empHrs*EMP_RATE_PER_HOUR;
+       System.out.println("emp wage:"+empWage);
 	}
 }
