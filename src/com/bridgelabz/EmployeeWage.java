0fcd1e7ad048f0cd4fcbd98161f16598971391da
Manikprabhu1999 <mbjewargi@gmail.com>
2022-10-30 17:54:53 +0530

    Added:Daily Wage Calculation

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -2,15 +2,23 @@ package com.bridgelabz;
 
 public class EmployeeWage {
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		// TODO Auto-generated method stub
         //Constants
-		int IS_FULL_TIME = 1;
+	    int empHrs;
+	    int empWage=0;
+		int IS_FULL_TIME=1;
+		int EMP_RATE_PER_HOUR=20;
 		//Computation
 double empCheck = Math.floor(Math.random() * 10) % 2;
 if (empCheck == IS_FULL_TIME)
-	System.out.println("Employee is present");
+	empHrs=8;
+	//System.out.println("Employee is present");
 else
-	System.out.println("Employee is Absent");
+	//System.out.println("Employee is Absent");
+	empHrs=0;
+empWage=empHrs*EMP_RATE_PER_HOUR;
+System.out.println("emp wage:"+empWage);
+
 	}
 }
