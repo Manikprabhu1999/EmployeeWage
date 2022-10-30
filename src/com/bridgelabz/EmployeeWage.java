e96bed6125e841c0e2a1e99f579587c81285b993
Manikprabhu1999 <mbjewargi@gmail.com>
2022-10-30 18:19:19 +0530

    Added:Daily Employee Wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -5,12 +5,20 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         //Constants
+		int empWage=0;
+		int empHrs;
+		int EMP_RATE_PER_HOUR=20;
 		int IS_FULL_TIME = 1;
 		//Computation
 double empCheck = Math.floor(Math.random() * 10) % 2;
 if (empCheck == IS_FULL_TIME)
-	System.out.println("Employee is present");
+	empHrs=8;
+	
 else
-	System.out.println("Employee is Absent");
+	empHrs=0;
+empWage=empHrs*EMP_RATE_PER_HOUR;
+System.out.println("emp wage:"+empWage);
+
+
 	}
 }
